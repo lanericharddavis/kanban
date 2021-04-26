580c83643de5f1eaf9b17d6210ee39b79f6a1c1c
Lane <lanericharddavis@gmail.com>
2021-04-26 14:12:28 -0600

    login works

--- kanban.client/src/env.js
+++ kanban.client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'lanerd.us.auth0.com'
+export const audience = 'https://myKanbanApi.com'
+export const clientId = 'MrpO1Gkua5NxFhPG5U28Lksb5xPTWGU7'
